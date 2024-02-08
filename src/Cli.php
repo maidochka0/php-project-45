8a7b2bf2ec7970b766dd8c772a64cc3a28f74bf4
maid <archiarchez@gmail.com>
2024-02-09 10:45:36 +1200

    fix require

--- src/Cli.php
+++ src/Cli.php
@@ -1,7 +1,7 @@
 <?php
 namespace Project1\Cli;
 
-require_once "/home/maid/project-1/vendor/autoload.php"; //загружается только локальная автозагрузка
+require_once "vendor/autoload.php"; //загружается только локальная автозагрузка
 
 use function cli\line;
 use function cli\prompt;
